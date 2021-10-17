2c4d662f3fd4c59b94f9537b2e4c9b29893cc02c
Greysonnn <25331809+Greysonnn@users.noreply.github.com>
2021-10-17 00:29:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ increment the value when btn is clicked
 change the count-el in html to reflect new value
 */
 
+let saveEl = document.getElementById('save-el')
 let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1
+    countEl.textContent = count
 }
 
 function save() {
+    let savedEntry = count + ' - '
+    saveEl.textContent += savedEntry
     console.log(count)
 }
+
